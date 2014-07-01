5ae2407d4099982d4c6d6aee129ca2b57e4a28e1
LubyRuffy <LubyRuffy@mail.com>
2014-07-01 18:39:51 +0800

    error tips when host error

--- app/controllers/userhost_controller.rb
+++ app/controllers/userhost_controller.rb
@@ -24,6 +24,7 @@ class UserhostController < ApplicationController
     @info = "提交成功，我们会尽快更新（一般5分钟内会自动更新，最长第二天能完成）！"
     @error = false
     @host = params['host']
+    @rawurl = @host
     @host = host_of_url(@host)
     #render :text => @host
     url = Domainatrix.parse(@host)
