39160358832958d121ee294aaafaed7b9af4e2dc
LubyRuffy <LubyRuffy@mail.com>
2014-07-10 11:25:23 +0800

    支持批量提交host, skip_before_filter :verify_authenticity_token ，api接口需要关闭token的要求

--- tools/db_link_crawler.rb
+++ tools/db_link_crawler.rb
@@ -21,14 +21,27 @@ end # Def end
 @p = Processor.new(@m)
 #@pool = Thread.pool(2)
 @id=902660
+
+#load id from file
+File.open(@root_path+"/id.txt", 'r') {|f|
+  max_id = 0
+  text = f.readline
+  max_id = text.strip.to_i if text
+  @id = max_id if max_id>@id
+}
+
 while true
   sql = "select * from subdomain where id>#{@id} limit 10"
   r = @m.mysql.query(sql)
   if r.size>0
     hosts = []
+    ids = []
+    puts "===================="
+    print "id: "
     r.each {|h|
       #@pool.process(h) { |h|
-        puts "======#{h['id']} -> #{h['host']}======"
+        #puts "======#{h['id']} -> #{h['host']}======"
+        print h['id'].to_s+" "
         arr = []
         h['body'].scan(/(http[s]?:\/\/.*?)[ \/\'\"\>]/).each{|x|
           arr << hostinfo_of_url(x[0].downcase) if x[0].size>8 && x[0].include?('.')
@@ -38,11 +51,17 @@ while true
         }
       #}
       @id=h['id']
-      write_to_file @id
+      ids << h['id']
+
     }
+
     uri = URI('http://www.fofa.so/api/addhostp')
-    res = Net::HTTP.post_form(uri, 'host' => hosts.uniq.join(','))
-    puts res.body
+    res = Net::HTTP.post_form(uri, 'host' => hosts.uniq!.join(','))
+    #puts "id:"+ids.join(",")
+    puts ""
+    puts "host count:"+hosts.size.to_s
+    puts "response:"+res.body
+    write_to_file ids.max
     #curl_line = "curl http://www.fofa.so/api/addhost?host=#{hosts.uniq.join(',')} >/dev/null 2>&1"
     #puts curl_line
     #`#{curl_line}`
