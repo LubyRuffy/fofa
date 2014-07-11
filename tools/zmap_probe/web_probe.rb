a4380a9b867a5779dbbb8778a4040d7853f7b734
LubyRuffy <LubyRuffy@mail.com>
2014-07-11 11:43:06 +0800

    增加zmap下的web_probe.rb工具，用于进行快速特征数据导入

---
+++ tools/zmap_probe/web_probe.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# 结合zmap查找服务并提交到fofa
+# sudo zmap -p 28017 -o - -N1000 | ./web_probe.rb 28017
+# sudo zmap -p 9200 -o - -N1000 | ./web_probe.rb 9200
+require 'thread/pool'
+require 'net/http'
+require 'uri'
+
+$port = 80
+$port = ARGV[0].to_i if ARGV.size>0
+puts "port is : #{$port}"
+STDOUT.sync = true
+STDIN.sync = true
+@pool = Thread.pool(100)
+while (s = $stdin.gets)
+  s.strip!
+  @pool.process(s) {|s|
+    begin
+      Net::HTTP.start(s, $port) do |http|
+        http.open_timeout = 10  # xxx: 間違い
+        http.read_timeout = 10
+
+        response = http.head('/')
+        if response.code && response.code.to_i<1000
+          addurl = "curl http://fofa.so/api/addhost?host=#{s}:#{$port}"
+          puts addurl
+          `#{addurl}`
+        end
+      end
+      rescue=>e
+    end
+  }
+end
+
+@pool.shutdown
