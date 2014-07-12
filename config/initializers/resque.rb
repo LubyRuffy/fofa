5015a5041d3a7ef03130b40fa09ac102fac2cc05
LubyRuffy <LubyRuffy@mail.com>
2014-07-12 12:20:42 +0800

    增加quick_process_host队列，递归出来的host直接进入快速处理器，不做任何数据库判断，且通过线程池进行批量处理。

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,4 +3,4 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 resque_config = YAML.load_file(rails_root + '/config/database.yml')
 Resque.redis = "#{resque_config[rails_env]['redis']['host']}:#{resque_config[rails_env]['redis']['port']}"
-Resque.logger = Logger.new(File.join(Rails.root, 'log', 'resque.log'), 'a')
+#Resque.logger = Logger.new(File.join(Rails.root, 'log', 'resque.log'), 'a')
