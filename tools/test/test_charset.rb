ca634bc45350d8e8ae35bc97ea5b2dd4e57a4de9
LubyRuffy <LubyRuffy@mail.com>
2014-07-14 19:14:28 +0800

    添加测试目录

--- tools/test/test_charset.rb
+++ tools/test/test_charset.rb
@@ -1,11 +1,19 @@
+#/usr/bin/env ruby
 @root_path = File.expand_path(File.dirname(__FILE__))
 require @root_path+"/../../app/jobs/module/httpmodule.rb"
-require "test/unit"
 
-class TestHttp< Test::Unit::TestCase
-  include HttpModule
+include HttpModule
 
-  def test_getutf8()
+gem "minitest"
+require 'minitest/unit'
+require 'minitest/autorun'
+
+class TestMeme < MiniTest::Unit::TestCase
+
+  def setup
+  end
+
+  def test_get_utf8
     http = get_web_content 'www.zaren.hu'
     http[:utf8html] = get_utf8 http[:html] if http[:html] and http[:html].size > 2
     assert(http[:utf8html].valid_encoding?)
