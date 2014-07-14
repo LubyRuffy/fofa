d107fcadb1dff81dee4ea949e6fafb288d1b5066
LubyRuffy <LubyRuffy@mail.com>
2014-07-14 14:47:46 +0800

    修复由于hostinfo_of_url返回nil导致报错的问题

--- app/jobs/module/lrlink.rb
+++ app/jobs/module/lrlink.rb
@@ -48,7 +48,7 @@ module Lrlink
   end
 
   def is_bullshit_host?(host)
-    $hosts = %w|.i.sohu.com .tumblr.com .soufun.com .ymjx168.com .ninemarket.com .12market.com .cailiao.com .taobao.com .blogfa.com .parsiblog.com .blog.ir .mihanblog.com .persianblog.ir .niniweblog.com .lapozz.hu .blogcu.com|
+    $hosts = %w|.i.sohu.com .tumblr.com .soufun.com .ymjx168.com .ninemarket.com .12market.com .cailiao.com .taobao.com .blogfa.com .parsiblog.com .blog.ir .mihanblog.com .persianblog.ir .niniweblog.com .lapozz.hu .blogcu.com .blogsky.com .deviantart.com|
     $hosts.each{|h|
       return true if host.include?(h)
     }
