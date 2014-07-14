d07376407de98e30a322fd0277d88b767462205d
LubyRuffy <LubyRuffy@mail.com>
2014-07-14 11:15:43 +0800

    排除垃圾ip

--- app/jobs/module/lrlink.rb
+++ app/jobs/module/lrlink.rb
@@ -43,7 +43,7 @@ module Lrlink
   end
 
   def is_bullshit_host?(host)
-    $hosts = %w|.i.sohu.com .tumblr.com .soufun.com .ymjx168.com|
+    $hosts = %w|.i.sohu.com .tumblr.com .soufun.com .ymjx168.com .ninemarket.com .12market.com|
     $hosts.each{|h|
       return true if host.include?(h)
     }
---
+++ tools/remove_bullship_ip.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+#删除数据库中作恶的ip对应的host（只保留根域名或www）
+require 'mysql2'
+#require 'thread/pool'
+@root_path = File.expand_path(File.dirname(__FILE__))
+require @root_path+"/../app/jobs/module/webdb2_class.rb"
+require @root_path+"/../app/jobs/module/lrlink.rb"
+include Lrlink
+
+
+
+@m = WebDb.new(@root_path+"/../config/database.yml")
+@bid_file = @root_path+"/bid.txt"
+@id=0
+@did=0
+
+#load id from file
+if File.exist?(@bid_file)
+  File.open(@bid_file, 'r') {|f|
+    max_id = 0
+    text = f.readline
+    max_id = text.strip.to_i if text
+    @id = max_id if max_id>@id
+  }
+end
+
+def write_to_file(id)
+  File.open(@bid_file, 'w') do |f|
+    f.puts id
+  end
+end # Def end
+
+while true
+  sql = "select id,ip,subdomain from subdomain where id>#{@id} limit 1000"
+  r = @m.mysql.query(sql)
+  if r.size>0
+    r.each {|h|
+      @id=h['id']
+      if h['ip'] && is_bullshit_ip?(h['ip']) && h['subdomain'].size>4
+        sql = "delete from subdomain where id=#{@id}"
+        #puts sql
+        @m.mysql.query(sql)
+        @did +=1
+        print "#{h['id']} : [deleted: #{@did}]\r"
+      end
+    }
+    print "#{@id} : [deleted: #{@did}]\r"
+    write_to_file @id
+  else
+    break
+  end
+
+end
