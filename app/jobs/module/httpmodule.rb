0b2d641289d3e82ad466ce0512bb080013cf2aeb
LubyRuffy <LubyRuffy@mail.com>
2014-07-15 18:54:26 +0800

    优化utf8编码

--- app/jobs/module/httpmodule.rb
+++ app/jobs/module/httpmodule.rb
@@ -256,6 +256,7 @@ module HttpModule
       encoding = "cp1251" if encoding.include?('1251') ||  encoding.include?('1250') #bug?
       encoding = "iso-8859-1" if encoding=='ISO-8855-1' || encoding=='IS0-8859-1' || encoding.include?('8859-1')  #bug?
       encoding = "iso-8859-2" if encoding=='ISO8859_2' #bug?
+      encoding = "euc-kr" if encoding=='KS_C_5601-1987' || encoding=='EUC_KR'
 
       if(encoding.to_s != "UTF-8")
         c = c.force_encoding(encoding)
