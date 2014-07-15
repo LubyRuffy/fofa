aac50208c06a490ee6cde8a38762aa5d5ab1090a
LubyRuffy <LubyRuffy@mail.com>
2014-07-15 11:48:57 +0800

    优化utf8编码

--- app/jobs/module/httpmodule.rb
+++ app/jobs/module/httpmodule.rb
@@ -248,15 +248,16 @@ module HttpModule
 
   def get_utf8(c,header=nil)
     encoding = GuessHtmlEncoding.guess(c,header)
-    #puts encoding
-    encoding = "GB2312" if encoding=='GBK2312' || encoding=='GB_2312-80' #bug?
-    encoding = "UTF-8" if encoding.include?('UTF') || encoding=='U1TF-8' || encoding=='UF-8' #bug?
-    encoding = "SHIFT_JIS" if encoding=='SHIFT-JIS' || encoding=='X-SJIS' || encoding=='SHFIT_JIS' || encoding=='SHIT-JIS' || encoding=='SHIFT_JS' || encoding='S-JIS' || encoding='SHIF_JIS' || encoding='SJIS-WIN' || encoding='S-JIS'#bug?
-    encoding = "cp1251" if encoding.include?('1251') ||  encoding.include?('1250') #bug?
-    encoding = "iso-8859-1" if encoding=='ISO-8855-1' || encoding='IS0-8859-1' || encoding.include?('8859-1')  #bug?
-    encoding = "iso-8859-2" if encoding=='ISO8859_2' #bug?
 
     if(encoding)
+      #puts encoding
+      encoding = "GB2312" if encoding=='GBK2312' || encoding=='GB_2312-80' #bug?
+      encoding = "UTF-8" if encoding.include?('UTF') || encoding=='U1TF-8' || encoding=='UF-8' #bug?
+      encoding = "SHIFT_JIS" if encoding=='SHIFT-JIS' || encoding=='X-SJIS' || encoding=='SHFIT_JIS' || encoding=='SHIT-JIS' || encoding=='SHIFT_JS' || encoding='S-JIS' || encoding='SHIF_JIS' || encoding='SJIS-WIN' || encoding='S-JIS'#bug?
+      encoding = "cp1251" if encoding.include?('1251') ||  encoding.include?('1250') #bug?
+      encoding = "iso-8859-1" if encoding=='ISO-8855-1' || encoding='IS0-8859-1' || encoding.include?('8859-1')  #bug?
+      encoding = "iso-8859-2" if encoding=='ISO8859_2' #bug?
+
       if(encoding.to_s != "UTF-8")
         c = c.force_encoding(encoding)
         c = c.encode('UTF-8', :undef => :replace, :invalid => :replace, :replace => '^')
