aaecd32a92a4a23198b9b6b8398e42ba84271738
LubyRuffy <LubyRuffy@gmail.com>
2014-07-19 20:54:35 +0800

    排除垃圾ip

--- tools/show_current.rb
+++ tools/show_current.rb
@@ -6,13 +6,24 @@ require @root_path+"/../app/jobs/module/lrlink.rb"
 include Lrlink
 
 @m = WebDb.new(@root_path+"/../config/database.yml")
+@last_array = []
 
 while true
-  puts "="*80
-  res= @m.mysql.query("select count(*) as cnt,ip,host,subdomain,domain,title from subdomain where id>(select max(id) from subdomain)-100000 and subdomain!='www' and subdomain!='' GROUP BY ip having cnt>100 order by cnt desc ")
+  @current_array= []
+  res= @m.mysql.query("select count(*) as cnt,ip,host,subdomain,domain,title from subdomain where id>(select max(id) from subdomain)-50000 and subdomain!='www' and subdomain!='' GROUP BY ip having cnt>100 order by cnt desc ")
   res.each{|r|
-    #puts r
-    printf("%-8s%-24s%-20s%-30s%-30s\n", r["cnt"], r["ip"], r["subdomain"], r["domain"], r["title"]) unless is_bullshit_host?(r["host"]) || is_bullshit_ip?(r["ip"])
+    @current_array << r
   }
+
+  #puts "="*80
+  @current_array.each{|r|
+    unless is_bullshit_host?(r["host"]) || is_bullshit_ip?(r["ip"])
+      if @last_array.select{|lr| r["ip"] == lr["ip"]}.empty?
+        printf("[%s] %-8s%-24s%-20s%-30s%-30s\n", Time.now, r["cnt"], r["ip"], r["subdomain"], r["domain"], r["title"])
+      end
+    end
+  }
+  @last_array = @current_array
+
   sleep 5
 end
