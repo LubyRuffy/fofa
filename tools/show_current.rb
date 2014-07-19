7c8b5ad66b9e71f01e712f26e926a8fea92e9e97
LubyRuffy <LubyRuffy@gmail.com>
2014-07-19 20:23:28 +0800

    排除垃圾ip

--- tools/show_current.rb
+++ tools/show_current.rb
@@ -2,10 +2,13 @@
 #通过数据库的body分析，来提取所有url，通过api提交到fofa（超过90天才更新）
 @root_path = File.expand_path(File.dirname(__FILE__))
 require @root_path+"/../app/jobs/module/webdb2_class.rb"
+require @root_path+"/../app/jobs/module/lrlink.rb"
+include Lrlink
 
 @m = WebDb.new(@root_path+"/../config/database.yml")
 
-res= @m.mysql.query("select count(*) as cnt,ip,subdomain,domain,title from subdomain where id>(select max(id) from subdomain)-50000 and subdomain!='www' and subdomain!='' GROUP BY ip order by cnt desc limit 30")
+res= @m.mysql.query("select count(*) as cnt,ip,host,subdomain,domain,title from subdomain where id>(select max(id) from subdomain)-50000 and subdomain!='www' and subdomain!='' GROUP BY ip order by cnt desc limit 30")
 res.each{|r|
-  printf("%-10s%-30s\n", r["cnt"], r["ip"])
+  #puts r
+  printf("%-8s%-24s%-30s%-40s\n", r["cnt"], r["ip"], r["domain"], r["title"]) unless is_bullshit_host?(r["host"]) || is_bullshit_ip?(r["ip"])
 }
