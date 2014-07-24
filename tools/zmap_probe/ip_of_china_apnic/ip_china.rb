850bfd404cb56c9d08d93785eb847b1cffdf8101
LubyRuffy <LubyRuffy@mail.com>
2014-07-24 14:27:16 +0800

    增加提取china的ip工具

--- tools/zmap_probe/ip_of_china_apnic/ip_china.rb
+++ tools/zmap_probe/ip_of_china_apnic/ip_china.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 require 'ipaddr'
-require 'netaddr'
 
 @root_path = File.expand_path(File.dirname(__FILE__))
 @apnic_file = File.join(@root_path,'delegated-apnic-latest')
-puts @apnic_file
+
 unless File.exist?(@apnic_file)
   `wget http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest`
   unless File.exist?(@apnic_file)
@@ -13,18 +12,33 @@ unless File.exist?(@apnic_file)
   end
 end
 
+#require 'netaddr'
+#ip_net_range = NetAddr.range("223.255.252.0", "223.255.253.255", :Inclusive => true, :Objectify => true)
+#cidr = NetAddr.merge(ip_net_range, :Objectify => true)
+#puts cidr
+#exit
+
+@cidrs = []
+STDOUT.sync=true
+STDERR.sync=true
 File.open(@apnic_file, 'r') {|f|
+  i = 0
   f.each_line{|l|
-    puts l
+    i+=1
+    $stderr.print "\r#{i}" if i%100==0
     if l.include?("apnic|CN|ipv4")
-      registry,cc,ip_type,start_ip,value,first_date,status = l.split("|")
-      start_ip_s=IPAddr.new(start_ip)
-      ip_from=start_ip_s.to_i
-      ip_to=ip_from+value.to_i-1
-      puts "#{start_ip} - #{IPAddr.new(ip_to,Socket::AF_INET).to_s}"
-      ip_net_range = NetAddr.range(ip_from, ip_to, :Inclusive => true, :Objectify => true)
-      cidrs = NetAddr.merge(ip_net_range, :Objectify => true)
-      puts cidrs
+        registry,cc,ip_type,start_ip,value,first_date,status = l.split("|")
+        cidr = "#{start_ip}/#{32-value.to_i.to_s(2).size+1}"
+        #puts cidr
+        #start_ip_s=IPAddr.new(start_ip)
+        #ip_from=start_ip_s.to_i
+        #ip_to=ip_from+value.to_i-1
+        #puts "#{start_ip} - #{IPAddr.new(ip_to,Socket::AF_INET).to_s}"
+        @cidrs << cidr
     end
   }
+}
+
+@cidrs.each{ |cidr|
+  $stdout.puts cidr
 }
