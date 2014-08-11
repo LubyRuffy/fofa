df4c61094d479ecf1da95d91899d21176314321f
LubyRuffy <LubyRuffy@mail.com>
2014-08-11 10:25:26 +0800

    修改redis池大小，服务器太多，每个sidekiq发起的连接数跟并发任务数有关，导致redis连接不上

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,15 +5,15 @@ config = YAML.load_file(rails_root + '/config/database.yml')[rails_env]['redis']
 redis_url = "redis://#{config['host']}:#{config['port']}/#{config['db']}"
 
 # Redis config
-Redis.current = ConnectionPool.new(size: (Sidekiq.server? ? 10 : 2), timeout: 5) do
-  Redis::Namespace.new(config['namespace'], :redis => Redis.new(:url => redis_url))
-end
+#Redis.current = ConnectionPool.new(size: (Sidekiq.server? ? 10 : 2), timeout: 5) do
+#  Redis::Namespace.new(config['namespace'], :redis => Redis.new(:url => redis_url))
+#end
 
 Sidekiq.configure_server do |cfg|
-  cfg.redis = Redis.current #{ :url => redis_url, :namespace => "#{config['namespace']}" }
+  cfg.redis = { :url => redis_url, :namespace => "#{config['namespace']}", :size => 10 }
   cfg.failures_max_count = false
 end
 
 Sidekiq.configure_client do |cfg|
-  cfg.redis = Redis.current # { :url => redis_url, :namespace => "#{config['namespace']}" }
+  cfg.redis = { :url => redis_url, :namespace => "#{config['namespace']}", :size => 2 }
 end
