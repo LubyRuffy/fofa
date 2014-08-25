f9cdbe7aa4ae72a66c79a454aabe4d883ee07a5a
LubyRuffy <LubyRuffy@mail.com>
2014-08-25 13:50:21 +0800

    增加ip配置，统一管理

---
+++ tools/check_mysql_status.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+require 'mysql2'
+require 'yaml'
+
+@root_path = File.expand_path(File.dirname(__FILE__))
+Dir.chdir @root_path
+
+rails_env = ENV['RAILS_ENV'] || 'development'
+config = YAML::load(File.open(@root_path+"/../config/database.yml"))[rails_env]
+ips = YAML::load(File.open(@root_path+"/../config/ips.yml"))['mysql_servers']
+
+ips.split(',').each_with_index{|h,i|
+  puts "========mysql server : #{h}========"
+  mysql = Mysql2::Client.new(:host => h, :username => config['username'],
+                                 :password => config['password'],
+                                 :port => config['port'], :secure_auth => config['secure_auth'],
+                                 :encoding => 'utf8', :reconnect => true)
+  mysql.query("show processlist").each do |row|
+    printf("%-8s%-8s%-25s%-10s %-30s %s\n",row['Id'],row['Time'],row['Host'],row['Command'],row['State'],row['Info']) unless row['Command']=='Sleep'
+  end
+}
--- tools/test/test_sphinx_agent.rb
+++ tools/test/test_sphinx_agent.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
+require 'yaml'
 
 @root_path = File.expand_path(File.dirname(__FILE__))
 Dir.chdir @root_path
+
+ips = YAML::load(File.open(@root_path+"/../../config/ips.yml"))['sphinx_servers']
 
 class String
   def string_between_markers marker1, marker2
@@ -10,7 +13,7 @@ class String
 end
 
 rs = []
-%w|10.108.72.81 10.108.72.37 10.108.72.105 10.108.72.108 10.108.72.109|.each_with_index{|h,i|
+ips.split(',').each_with_index{|h,i|
   info = `./test.py -h #{h} -p 9312 -i idx1p#{i} test -l 1`.string_between_markers('times in ',' documents')
   rs << "------> #{h} => #{info}"
 }
