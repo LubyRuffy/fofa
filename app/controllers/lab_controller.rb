a8111571450be59da1d9d462a64f7a988139af21
LubyRuffy <LubyRuffy@mail.com>
2014-09-05 18:19:31 +0800

    增加实验室工具：查找域名对应的ip范围段

--- app/controllers/lab_controller.rb
+++ app/controllers/lab_controller.rb
@@ -2,10 +2,10 @@ class LabController < ApplicationController
   def ips
     @domain = params['domain']
     @ips = Subdomain.connection.execute(%Q{
-        select INET_NTOA(INET_ATON(ip) & 0xFFFFFF00) as net,GROUP_CONCAT(hosts) as hosts,GROUP_CONCAT(ip) as ips,count(*) as cnt from(
-        select ip,GROUP_CONCAT(host) as hosts from subdomain
-        where reverse_domain=reverse(#{Subdomain.connection.quote(@domain)}) group by ip
-        )t group by net order by cnt desc
+select INET_NTOA(INET_ATON(ip) & 0xFFFFFF00) as net,GROUP_CONCAT(hosts) as hosts,GROUP_CONCAT(ip) as ips,count(*) as cnt from(
+	select ip,GROUP_CONCAT(host) as hosts from (select ip, host from subdomain
+	where reverse_domain=reverse(#{Subdomain.connection.quote(@domain)}) limit 10000) t group by ip
+)t group by net order by cnt desc,net asc
       })
 
   end
