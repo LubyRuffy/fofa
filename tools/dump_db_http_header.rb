ced4357953779d7c8f0f85e9a08ed28269083042
LubyRuffy <LubyRuffy@mail.com>
2014-09-10 15:24:14 +0800

    增加分析httpheader的工具，用于提取特征

---
+++ tools/dump_db_http_header.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+require 'yaml'
+require 'json'
+require 'erb'
+require 'active_record'
+require 'thinking_sphinx'
+
+@root_path = File.expand_path(File.dirname(__FILE__))
+require @root_path+"/../app/workers/module/httpmodule.rb"
+require @root_path+"/../app/workers/module/webdb2_class.rb"
+
+require @root_path+"/../app/helpers/search_helper.rb"
+include SearchHelper
+require @root_path+"/../app/models/subdomain.rb"
+require @root_path+"/../app/models/rule.rb"
+require @root_path+"/../app/models/charts.rb"
+
+Dir.chdir @root_path+"/../"
+puts "working dir: #{Dir.pwd}"
+
+rails_env = ENV['RAILS_ENV'] || 'development'
+thinking_config = YAML::load(File.open(@root_path+"/../config/thinking_sphinx.yml"))['development']
+
+config = YAML::load(File.open(@root_path+"/../config/database.yml"))[rails_env]
+ActiveRecord::Base.establish_connection (config)
+
+USE_THINKING_SPHINX=true
+if USE_THINKING_SPHINX
+  ThinkingSphinx::SphinxQL.functions!
+  #ThinkingSphinx::Middlewares::DEFAULT.delete ThinkingSphinx::Middlewares::UTF8
+  ThinkingSphinx::Configuration.instance.searchd.address = thinking_config['address']
+  ThinkingSphinx::Configuration.instance.searchd.port = thinking_config['port']
+else
+  @mysql ||= Mysql2::Client.new(:host => thinking_config['address'],
+                                :username => thinking_config['connection_options']['username'],
+                                :password => thinking_config['connection_options']['password'],
+                                :database => thinking_config['connection_options']['database'],
+                                :port => thinking_config['mysql41'],
+                                :encoding => 'utf8', :reconnect => true)
+end
+
+def query(query_info, max_id)
+  headers = []
+  match_query =  SphinxProcessor.parse(query_info)
+
+  options = {:match_mode => :extended, :index => 'subdomain_core',
+             :with => {:id => max_id..9999999999},
+             :sql => { :select => 'id,header,host'}, :per_page => 1000,
+             :page => 1, :order => "id asc"}
+  ThinkingSphinx.search(match_query, options).each{|r|
+    headers << r.header
+    max_id = r.id
+  }
+  [headers, max_id.to_i]
+end
+
+$maxid = 0
+$keys = []
+$known_keys = %w|Last-Modified Date Content-Length Connection Cache-Control Content-Location Last-Modified Etag Transfer-Encoding Content-Encoding Vary Expires Pragma Content-Language Location|
+while 1
+  headers,$maxid = query('host=".gov.cn"', $maxid)
+  unless headers.size>0
+    break
+  end
+
+  headers.each{|header|
+    header.split(/[\r\n]/).each{|l|
+      if l.include? ':'
+        k,v = l.split(':')
+        unless $keys.include?(k) || $known_keys.include?(k)
+          $keys << k
+          puts l
+        end
+      end
+    }
+  }
+
+  puts "-----", $maxid
+end
+
+
