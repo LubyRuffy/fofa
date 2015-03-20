f1959c0fefec984d522136f1a5d41886c30a73a8
LubyRuffy <LubyRuffy@gmail.com>
2015-03-20 18:39:43 +0800

    upgrade tools of get hosts from google

--- tools/get_domain_from_google.rb
+++ tools/get_domain_from_google.rb
@@ -18,7 +18,7 @@ require 'ip'
 trap "SIGINT", 'exit'
 
 
-def get_host_from_google(query)
+def get_domain_from_google(query)
   subdomains = []
   (1..100).each{|n|
     print '.'
@@ -26,18 +26,15 @@ def get_host_from_google(query)
     added = false
     added_arr = []
 
-    ext_host = ''
-    ext_host = '%20-inurl:' + subdomains.join('%20-inurl:')  if subdomains.size>0
-    url = "https://www.google.com.hk/search?hl=en&lr=&ie=UTF-8&q=site:#{query}#{ext_host}&start=" + (n*100).to_s + "&sa=N&filter=0&num=100"
-    #url = "https://www.google.com.hk/search?hl=en&lr=&ie=UTF-8&q=site:" + query + "&start=" + (n*100).to_s + "&sa=N&filter=0&num=100"
-    puts "===get "+url
+    url = "https://www.google.com.hk/search?hl=en&lr=&ie=UTF-8&q=" + query + "&start=" + (n*100).to_s + "&sa=N&filter=0&num=100"
+    #puts url
     get_linkes(open(url).read).select{|l|
       domain_info = get_domain_info_by_host(l)
       return false unless domain_info
       true
     }.each{|l|
       hostinfo = hostinfo_of_url(l)
-      if !subdomains.include?(hostinfo) && hostinfo.downcase.include?(query.downcase)
+      if !subdomains.include?(hostinfo)
         added = true
         added_arr << hostinfo
         subdomains << hostinfo
@@ -58,6 +55,6 @@ def addhost(hosts)
   puts "response:"+res.body
 end
 
-hosts = get_host_from_google(ARGV[0])
+hosts = get_domain_from_google(ARGV[0])
 puts hosts.size
 addhost(hosts)
