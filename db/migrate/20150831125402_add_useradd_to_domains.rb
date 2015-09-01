37894a7ca3cf05903738d52c62773a4e0e512a3d
LubyRuffy <lubyruffy@gmail.com>
2015-09-02 01:17:12 +0800

    修正命名错误

--- db/migrate/20150831125402_add_useradd_to_domains.rb
+++ db/migrate/20150831125402_add_useradd_to_domains.rb
@@ -1,4 +1,4 @@
-class AddUseraddToPersons < ActiveRecord::Migration
+class AddUseraddToDomains < ActiveRecord::Migration
   def change
     add_column :asset_domains, :useradd, :boolean
     add_column :asset_hosts, :useradd, :boolean
